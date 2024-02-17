ddd5611f618951a27d45193ea0f3f299243323aa
Sai Kumar <saikumarponnaganti99@gmail.com>
2024-02-17 18:08:02 +0530

    no errors logistics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Ecommerce from './pages/Ecommerce'
+import Logistics from './pages/Logistics'
 import { ThemeProvider } from '@mui/material/styles'
 import getTheme from './theme'
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 function App() {
   return (
     <ThemeProvider theme={getTheme('light')}>
-    <Ecommerce />
+    <Logistics />
     </ThemeProvider>
   );
 }
