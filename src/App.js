cf1a2b6dc32c4858c0710623e043949cc3e641d5
unknown <kaushik4759@gmail.com>
2024-02-17 18:52:19 +0530

    coworking

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Coworking from './components/Coworking'
+import { ThemeProvider } from '@mui/material/styles'
+import getTheme from './theme'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={getTheme('light')}>
+      <Coworking />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App 
