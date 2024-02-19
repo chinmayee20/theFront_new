2894ee05715d230caafdd14fb0a1f51a7882e1e2
unknown <kaushik4759@gmail.com>
2024-02-19 15:22:14 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import CareerListing from './components/CareerListing'
 import { ThemeProvider } from '@mui/material/styles'
 import getTheme from './theme'
+import Page from './components/Page'
 
 function App() {
   return (
     <ThemeProvider theme={getTheme('light')}>
+      <Page>
       <CareerListing />
+      </Page>
     </ThemeProvider>
   )
 }
