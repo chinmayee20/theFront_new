56898972157d2e14f4272e0ac17172fd814ff918
Sai Kumar <saikumarponnaganti99@gmail.com>
2024-02-19 15:09:45 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import Coworking from './components/Coworking'
 import { ThemeProvider } from '@mui/material/styles'
 import getTheme from './theme'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import Startup from './components/Startup'
+import Page from './components/Page';
+
 
 function App() {
   return (
+    <Page>
     <ThemeProvider theme={getTheme('light')}>
-      <Coworking />
+      <Startup />
     </ThemeProvider>
+    </Page>
   )
 }
 
