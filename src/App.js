ec49c33911451d99035b63706dd32f45028d3b9d
unknown <kaushik4759@gmail.com>
2024-02-19 13:27:56 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Service from './components/Service'
 import { ThemeProvider } from '@mui/material/styles'
 import getTheme from './theme'
+import Page from './components/Page'
 
 function App() {
   return (
     <ThemeProvider theme={getTheme('light')}>
+      <Page>
       <Service />
+      </Page>
     </ThemeProvider>
   )
 }
