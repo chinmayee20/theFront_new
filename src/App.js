f5ccaa645acf38855d53349e9818a58d6921f85d
unknown <kaushik4759@gmail.com>
2024-02-19 12:54:52 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { ThemeProvider } from '@mui/material/styles'
 import getTheme from './theme'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Page from './components/Page'
 
 function App() {
   return (
     <ThemeProvider theme={getTheme('light')}>
+      <Page>
       <Enterprise />
+      </Page>
     </ThemeProvider>
   )
 }
