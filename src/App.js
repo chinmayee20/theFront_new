1077240fc2007d59178d72fcee230eb470ca32a6
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-02-19 12:56:43 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import CloudHosting from "./components/CloudHosting";
 import { ThemeProvider } from "@mui/material/styles";
 import getTheme from "./theme";
+import Page from "./components/Page";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 function App() {
   return (
     <ThemeProvider theme={getTheme("light")}>
-      <CloudHosting />
+      <Page>
+        <CloudHosting />
+      </Page>
     </ThemeProvider>
   );
 }
