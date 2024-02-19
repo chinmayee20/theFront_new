c47ebdbbd6f560d51f8dadfb8575cad2cc3c80aa
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-02-19 13:00:48 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import WebBasic from "./components/WebBasic";
 import { ThemeProvider } from "@mui/material/styles";
 import getTheme from "./theme";
+import Page from "./components/Page";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <ThemeProvider theme={getTheme("light")}>
-      <WebBasic />
+      <Page>
+        <WebBasic />
+      </Page>
     </ThemeProvider>
   );
 }
