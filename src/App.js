f51712a135d2eba3cac56a039a5aeb1022c9b035
unknown <kaushik4759@gmail.com>
2024-02-19 15:31:18 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Coworking from './components/Coworking'
 import { ThemeProvider } from '@mui/material/styles'
 import getTheme from './theme'
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Page from "components/Page"
 
 function App() {
   return (
     <ThemeProvider theme={getTheme('light')}>
+      <Page>
       <Coworking />
+      </Page>
     </ThemeProvider>
   )
 }
