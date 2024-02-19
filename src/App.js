68f2aac640c7ddc6e92c6311aea795a2dd4eea36
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-02-19 13:04:35 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Rental from "./components/Rental";
 import { ThemeProvider } from "@mui/material/styles";
 import getTheme from "./theme";
+import Page from "./components/Page";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
     <ThemeProvider theme={getTheme("light")}>
-      <Rental />
+      <Page>
+        <Rental />
+      </Page>
     </ThemeProvider>
   );
 }
