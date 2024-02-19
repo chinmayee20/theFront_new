0bfc50b101d8e47fc79b1cc6b49533f0258cd220
Sai Kumar <saikumarponnaganti99@gmail.com>
2024-02-19 17:52:09 +0530

    helpcenter_overview

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@mui/material/styles'
 import getTheme from './theme'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import ContactPage from './components/ContactPage/ContactPage'
+import HelpCenter from './components/HelpCenter/HelpCenter'
 import Page from './components/Page';
 
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <Page>
     <ThemeProvider theme={getTheme('light')}>
-      <ContactPage />
+      <HelpCenter />
     </ThemeProvider>
     </Page>
   )
