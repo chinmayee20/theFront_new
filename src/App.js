c53090d34046cd5eac85420bcc4e528c4d954eb0
Sai Kumar <saikumarponnaganti99@gmail.com>
2024-02-19 15:15:19 +0530

    pricing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@mui/material/styles'
 import getTheme from './theme'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import Startup from './components/Startup'
+import Pricing from './components/Pricing'
 import Page from './components/Page';
 
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <Page>
     <ThemeProvider theme={getTheme('light')}>
-      <Startup />
+      <Pricing />
     </ThemeProvider>
     </Page>
   )
