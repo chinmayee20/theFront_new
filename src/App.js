1b4712ff7778e0fa56cb8f52ac6983bcc8dafcae
unknown <kaushik4759@gmail.com>
2024-02-19 15:28:08 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import DesktopApp from './components/DesktopApp'
 import { ThemeProvider } from '@mui/material/styles'
 import getTheme from './theme'
+import Page from './components/Page'
 
 function App() {
   return (
     <ThemeProvider theme={getTheme('light')}>
+      <Page>
       <DesktopApp />
+      </Page>
     </ThemeProvider>
   )
 }
