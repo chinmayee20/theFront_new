c832cdcfe1bfb89cd167ef2a9ec4c10911e2d378
kaushik59 <152191622+kaushik59@users.noreply.github.com>
2024-02-20 11:47:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from '@mui/material/styles'
 import getTheme from './theme'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import Page from "./components/Page"
+import Page from "../components/Page"
 
 function App() {
   return (
