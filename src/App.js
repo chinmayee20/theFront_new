0fd15bc8e5bbb5f55a5135cc756391d6c4b52926
unknown <kaushik4759@gmail.com>
2024-02-20 12:19:07 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import PortfolioPage from './components/PortfolioPage'
+import { ThemeProvider } from '@mui/material/styles'
+import getTheme from './theme'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={getTheme('light')}>
+      <PortfolioPage />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App 
